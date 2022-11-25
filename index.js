e2874e97d6505dfa80550bac5fbfe08d2dd70f9a
your <omar.hasan3894@gmail.com>
2022-11-25 23:54:18 +0200

    t

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { features } = require("process");
-
 fetch('https://retry202103069.herokuapp.com/service')
   .then(response => response.json())
   .then(json => {
@@ -27,55 +25,51 @@ json.services.forEach(e => {
 
 
 
+     })
+   
+   
+    // for features
 
-                                  // for features
 
-
-                                  mainContainer2 = document.getElementById('featued');
-                                  console.log(json.services)
-                                  
-                                    
-                                      
-                                   var originalContent2 = ` 
-                                  
-                                   <div class="card">
-                                     <img src="assets/img/featured-1.jpg" class="card-img-top" alt="...">
-                                     <div class="card-body">
-                                       <h5 class="card-title"><a href="">[TITLE2]</a></h5>
-                                       <p class="card-text">[DESCRIPTION2]</p>
-                                       <a href="#" class="btn">Explore more</a>
-                                     </div>
-                                   </div>
-                                   
-                                   `;
-                               
-                               json.features.forEach(e => {
-                                 var currentContent2 = originalContent2;
-                                 currentContent2 = currentContent2.replace(`[TITLE2]`,e["A"])
-                                 currentContent2 = currentContent2.replace(`[DESCRIPTION2]`,e["B"])
-                                // currentContent2 = currentContent2.replace(`[ICON2]`,e["C"])
-                               
-                                 var div2 = document.createElement('div')
-                                 div2.className = "col-lg-4 col-md-6 d-flex align-items-stretch mb-5 mb-lg-0"
-                                 div2.innerHTML = currentContent;
-                                 mainContainer2.appendChild(div);
+    mainContainer2 = document.getElementById('featued');
+    console.log(json.services)
+    
+      
+        
+     var originalContent2 = ` 
+    
+     <div class="card">
+       <img src="assets/img/featured-1.jpg" class="card-img-top" alt="...">
+       <div class="card-body">
+         <h5 class="card-title"><a href="">[TITLE2]</a></h5>
+         <p class="card-text">[DESCRIPTION2]</p>
+         <a href="#" class="btn">Explore more</a>
+       </div>
+     </div>
+     
+     `;
+ 
+ json.features.forEach(e => {
+   var currentContent2 = originalContent2;
+   currentContent2 = currentContent2.replace(`[TITLE2]`,e["A"])
+   currentContent2 = currentContent2.replace(`[DESCRIPTION2]`,e["B"])
+  // currentContent2 = currentContent2.replace(`[ICON2]`,e["C"])
+ 
+   var div2 = document.createElement('div')
+   div2.className = "col-lg-4 col-md-6 d-flex align-items-stretch mb-5 mb-lg-0"
+   div2.innerHTML = currentContent;
+   mainContainer2.appendChild(div);
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-     })
-
-  
+   
+   
+   
+   
+   
+   
+    }
+    
+    
+    );
