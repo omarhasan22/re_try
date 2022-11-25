d7d9c801129f1db1fb8ead1098666993d4d665c2
your <omar.hasan3894@gmail.com>
2022-11-26 00:09:37 +0200

    t

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 fetch('https://retry202103069.herokuapp.com/service')
   .then(response => response.json())
   .then(json => {
-    mainContainer = document.getElementById('mainContainer');
-   console.log(json.services)
    
-     
-       
-    var originalContent = ` 
-                            <div class="icon"><i class="[ICON]"></i></div>
-                            <h4 class="title"><a href="">[TITLE]</a></h4>
-                            <p class="description">[DESCRIPTION]</p>`;
-
-json.services.forEach(e => {
-  var currentContent = originalContent;
-  currentContent = currentContent.replace(`[TITLE]`,e["A"])
-  currentContent = currentContent.replace(`[DESCRIPTION]`,e["B"])
-  currentContent = currentContent.replace(`[ICON]`,e["C"])
-
-  var div = document.createElement('div')
-  div.className = "col-lg-4 col-md-6 icon-box"
-  div.innerHTML = currentContent;
-  mainContainer.appendChild(div);
-
-
-
-
-     })
-
 
     // for features
 
@@ -64,7 +39,32 @@ json.services.forEach(e => {
 
 })
 
-   
+mainContainer = document.getElementById('mainContainer');
+console.log(json.services)
+
+  
+    
+ var originalContent = ` 
+                         <div class="icon"><i class="[ICON]"></i></div>
+                         <h4 class="title"><a href="">[TITLE]</a></h4>
+                         <p class="description">[DESCRIPTION]</p>`;
+
+json.services.forEach(e => {
+var currentContent = originalContent;
+currentContent = currentContent.replace(`[TITLE]`,e["A"])
+currentContent = currentContent.replace(`[DESCRIPTION]`,e["B"])
+currentContent = currentContent.replace(`[ICON]`,e["C"])
+
+var div = document.createElement('div')
+div.className = "col-lg-4 col-md-6 icon-box"
+div.innerHTML = currentContent;
+mainContainer.appendChild(div);
+
+
+
+
+  })
+
    
     });
 
